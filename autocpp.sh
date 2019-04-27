45a858fe481f05f550ff0d7707e72bd401bd00c3
Aditya <k.aditya00@gmail.com>
2019-04-27 23:42:09 +0530

    [Feat] Longest path between 2 leafs
    - Calculate longest path between 2 leaf nodes in an undirected graph
    - Update autocpp.sh script to output the ans into a file

--- autocpp.sh
+++ autocpp.sh
@@ -1,7 +1,7 @@
 g++ -o program $1
 start=$(($(date +%s%N)/1000000))
-./program < input.txt
-end=$(($(date +%s%N)/1000000))
-echo "Time of execution: $(($end - $start))"
+ans=`./program < input.txt`
+# end=$(($(date +%s%N)/1000000))
+echo "$ans" > output.txt
+# echo "\nTime of execution: $(($end - $start))"
 rm program
-

0aa14e9a2172fa69a06508d06dd461cf9d8b5748
Aditya Kumar <k.aditya00@gmail.com>
2019-07-25 02:36:17 +0530

    [Feat] LCA, boundary & level order traversal

--- Trees/LevelOrderTraversal.cpp
+++ Trees/LevelOrderTraversal.cpp
@@ -15,6 +15,8 @@ struct Node {
 
     static void LevelOrderTraversalWithSpace(Node *node, bool printNext);
     static void ConnectSameLevelNodesWithSpace(Node *node);
+    static li DifferenceOddEvenLevelNodes1(Node *node);
+    static li DifferenceOddEvenLevelNodes2(Node *node);
 };
 
 void Node::LevelOrderTraversalWithSpace(Node *node, bool printNext=false) {
@@ -58,15 +60,55 @@ void Node::ConnectSameLevelNodesWithSpace(Node *node) {
     }
 }
 
+li Node::DifferenceOddEvenLevelNodes1(Node *node) {
+    queue<Node*> q;
+    q.push(node);
+
+    Node *temp = NULL;
+    li sumEven = 0, sumOdd = 0;
+    bool even = true;
+    while(!q.empty()) {
+        li size = q.size();
+
+        while(size--) {
+            temp = q.front();
+            q.pop();
+
+            if(even) sumEven += temp->data;
+            else sumOdd += temp->data;
+        
+            if(temp->left != NULL) q.push(temp->left);
+            if(temp->right != NULL) q.push(temp->right);
+        }
+        even = !even;
+    }
+
+    return sumEven - sumOdd;
+}
+
+li Node::DifferenceOddEvenLevelNodes2(Node* node) {
+    if(node == NULL) return 0;
+    return node->data
+            - DifferenceOddEvenLevelNodes2(node->left)
+            - DifferenceOddEvenLevelNodes2(node->right);
+}
+
 int main() {
-    Node *root = new Node(5);
-    root->left = new Node(4);
-    root->right = new Node(6);
-    root->right->left = new Node(3);
-    root->right->right = new Node(8);
+Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->left = new Node(7);
+    root->right->right = new Node(6);
+    root->right->right->left = new Node(8);
+    root->right->right->right = new Node(9);
 
     Node::ConnectSameLevelNodesWithSpace(root);
     Node::LevelOrderTraversalWithSpace(root);
     Node::LevelOrderTraversalWithSpace(root, true);
+
+    cout << "Difference between odd & even levels: " << Node::DifferenceOddEvenLevelNodes1(root) << endl;
+    cout << "Difference between odd & even levels: " << Node::DifferenceOddEvenLevelNodes2(root) << endl;
     return 0;
 }

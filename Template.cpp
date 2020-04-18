f0f4a2cc64cbdc73332c4e33ffa511200226df69
Aditya <k.aditya00@gmail.com>
2020-04-18 18:39:19 +0530

    feat(trie): Template for Trie
    - Add git action to verify commit syntax

---
+++ Template.cpp
@@ -0,0 +1,21 @@
+// adityaa30
+#include <bits/stdc++.h>
+#define int long long int
+#define debug(a) cout << #a << "=" << a << ' ';
+using namespace std;
+
+const int MOD = 1000000007;
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t = 1;
+  cin >> t;
+  for(int test = 1; test <= t; ++test) {
+
+  }
+
+  return 0;
+}

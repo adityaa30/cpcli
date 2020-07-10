b3efc3f630ca34802a52133a6f3b5f42b084fa14
Aditya <k.aditya00@gmail.com>
2020-07-11 04:37:49 +0530

    test: check all cpp files compile successfully

---
+++ test-cpp-compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+IFS=$'\n'; set -f
+
+CPP_FILES=`find . -name '*.cpp'`
+TEST_PROG_FILE=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo ''`
+
+TEST_EXIT_STATUS=0
+
+echo "Checking if all *.cpp files compiles successfully 🙃"
+
+for path in $CPP_FILES
+do
+    g++ $path -o $TEST_PROG_FILE
+
+    if [ -f $TEST_PROG_FILE ];
+    then
+        echo "$path: ✅"
+        rm $TEST_PROG_FILE
+    else
+        echo "$path: ❌"
+        TEST_EXIT_STATUS=1
+    fi
+done
+
+unset IFS; set +f
+
+exit $TEST_EXIT_STATUS

2a151c00e311295967e7d12a894fc4e9c9b507b5
Aditya <k.aditya00@gmail.com>
2020-08-29 19:10:11 +0530

    chore: Competitive programming template

--- Template.cpp
+++ Template.cpp
@@ -1,19 +1,82 @@
 // adityaa30
 #include <bits/stdc++.h>
+#define var(x) #x, x
 #define int long long int
-#define print1(a) cout << '(' << #a << '=' << a << ')';
-#define print2(a, b)                                                           \
-  cout << '(' << #a << '=' << a << ',' << #b << '=' << b << ')';
-#define print3(a, b, c)                                                        \
-  cout << '(' << #a << '=' << a << ',' << #b << '=' << b << ',' << #c << '='   \
-       << c << ')';
-#define print(it)                                                              \
-  cout << #it << " -> ";                                                       \
-  for (auto __x__ : it)                                                        \
-    cout << __x__ << ' ';                                                      \
-  cout << '\n';
 using namespace std;
-const int MOD = 1000000007;
+
+template <typename A, typename B> string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string &s) { return '"' + s + '"'; }
+string to_string(const char *s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "true" : "false"); }
+string to_string(char c) { return to_string(string(1, c)); }
+
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+
+template <size_t N> string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+
+template <typename A> string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A, typename B> string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
+         to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
+         to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void print() { cout << endl; }
+
+template <typename Head, typename... Tail> void print(Head H, Tail... T) {
+  cout << " " << to_string(H);
+  print(T...);
+}
+
+const int MOD = 1e9 + 7;
 
 int PosX[] = {0, 1, 0, -1, 1, 1, -1, -1};
 int PosY[] = {1, 0, -1, 0, 1, -1, 1, -1};

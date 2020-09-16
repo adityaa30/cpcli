ff8315916865b1fe88d2a19035be4741aa08b15b
Aditya Kumar <k.aditya00@gmail.com>
2020-09-16 14:37:12 +0530

    test: setup tox tests for security, flake8

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+install_requires = [
+    'lxml>=4.5.0'
+]
+
+setup(
+    name='cp-cli',
+    version='1.0',
+    description='Competitive Programming CLI',
+    long_description=open('README.md').read(),
+)
